b93765f2cf13573891dbb0e0e37e45936d32799a
Ariel Gama <148490113+marielgama@users.noreply.github.com>
2025-09-25 10:34:48 +0000

    inclusao da peça cavalo

---
+++ xadrez-2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+int main() {
+    
+   int t; 
+   int b = 1;
+   int r = 1;
+   int c = 1;
+
+   
+   // Torre
+   printf("\n");
+   printf("O movimento da peça Torre foi: \n");
+   for (t = 1; t <= 5; t++) {
+        printf("Direita\n");
+   }
+   
+   // Bispo
+   printf("\n");
+   printf("O movimento da peça Bispo foi: \n");
+
+   while (b <= 5) 
+    {
+        printf("Cima Direita\n");
+        b++;
+    }
+ 
+
+    // Rainha
+    printf("\n");
+    printf("O movimento da peça Rainha foi: \n");
+
+    do {
+        printf("Esquerda\n");
+        r++;
+    } while (r <= 8); 
+
+
+    // Cavalo
+    printf("\n");
+    printf("O movimento da peça Cavalo foi: \n");
+
+    while (c--) 
+        {
+            for (int d = 0; d < 2; d++) {
+                printf("Baixo\n");
+            }
+            printf("Esquerda\n");
+        }
+    
+    
+
+    return 0;
+
+}
